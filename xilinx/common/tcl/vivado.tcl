9593e5eee6a697b3668816d6bdbdbc17b420133d
Richard Xia <rxia@sifive.com>
2017-10-03 18:24:18 -0700

    Restructure Tcl script entrypoint.
    vivado.tcl is now the entrypoint for the Vivado Tcl scripts and will
    automatically source all the other required scripts.
    
    A command line argument parser was written and replaces the previous system of
    using environment variables to pass values into the scripts. The VSRCSVIVADOTCL
    environment variable has been replaced with a -F command line option, and the
    file format has changed from a Tcl script to a simple newline-delimited list of
    files.

--- xilinx/common/tcl/vivado.tcl
+++ xilinx/common/tcl/vivado.tcl
@@ -1,11 +1,20 @@
 # See LICENSE for license details.
 
+# Set the variable for the directory that includes all scripts
+set scriptdir [file dirname [info script]]
+
+# Set up variables and Vivado objects
+source [file join $scriptdir "prologue.tcl"]
+
+# Initialize Vivado project files
+source [file join $scriptdir "init.tcl"]
+
 # Synthesize the design
 source [file join $scriptdir "synth.tcl"]
 
 # Pre-implementation debug
-if {[info exists ::env(PRE_IMPL_DEBUG_TCL)]} {
-  source [file join $scriptdir $::env(PRE_IMPL_DEBUG_TCL)]
+if {[info exists pre_impl_debug_tcl]} {
+  source [file join $scriptdir $pre_impl_debug_tcl]
 }
 
 # Post synthesis optimization
@@ -21,8 +30,8 @@ source [file join $scriptdir "route.tcl"]
 source [file join $scriptdir "bitstream.tcl"]
 
 # Post-implementation debug
-if {[info exists ::env(POST_IMPL_DEBUG_TCL)]} {
-  source [file join $scriptdir $::env(POST_IMPL_DEBUG_TCL)]
+if {[info exists post_impl_debug_tcl)]} {
+  source [file join $scriptdir $post_impl_debug_tcl]
 }
 
 # Create reports for the current implementation
