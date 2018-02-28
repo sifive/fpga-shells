ce12f9965888ae41971ae3b6003d31867984e0ff
Cyril-Jean <cyril.s.jean@gmail.com>
2018-02-28 20:32:48 +0000

    Added configuration parameters for GL1 and Gl2.

--- src/main/scala/ip/microsemi/polarfire_ccc/PolarFireCCC.scala
+++ src/main/scala/ip/microsemi/polarfire_ccc/PolarFireCCC.scala
@@ -10,8 +10,12 @@ import freechips.rocketchip.config._
 case class PolarFireCCCParameters(
   name:             String,
   pll_in_freq:      Double  = 50,
+  gl0Enabled:       Boolean = true,
+  gl1Enabled:       Boolean = false,
+  gl2Enabled:       Boolean = false,
   gl0_0_out_freq:   Double  = 111.111,
-  gl1Enabled:       Boolean = false
+  gl1_0_out_freq:   Double  = 111.111,
+  gl2_0_out_freq:   Double  = 111.111
 )
 
 // Black Box for Microsemi PolarFire Clock Conditioning Circuit (CCC) Actel:SgCore:PF_CCC:1.0.112
@@ -42,7 +46,15 @@ class PolarFireCCC(c : PolarFireCCCParameters)(implicit val p:Parameters) extend
 create_design -id Actel:SgCore:PF_CCC:1.0.112 -design_name {""" ++ modulename ++"""} -config_file {} -params {} -inhibit_configurator 0
 open_smartdesign -design {""" ++ modulename ++"""}
 configure_design -component {""" ++ modulename ++"""} -library {} 
-configure_vlnv_instance -component {""" ++ modulename ++"""} -library {} -name {""" ++ modulename ++"""_0} -params {"PLL_IN_FREQ_0:""" ++ c.pll_in_freq.toString ++ """" "GL0_0_OUT_FREQ:""" ++ c.gl0_0_out_freq.toString ++ """"} -validate_rules 0 
+configure_vlnv_instance -component {""" ++ modulename ++"""} -library {} -name {""" ++ modulename ++"""_0} \
+    -params {"PLL_IN_FREQ_0:""" ++ c.pll_in_freq.toString ++ """" \
+             "GL0_0_IS_USED:""" ++ c.gl0Enabled.toString ++ """" \
+             "GL0_0_OUT_FREQ:""" ++ c.gl0_0_out_freq.toString ++ """" \
+             "GL1_0_IS_USED:""" ++ c.gl1Enabled.toString ++ """" \
+             "GL1_0_OUT_FREQ:""" ++ c.gl1_0_out_freq.toString ++ """" \
+             "GL2_0_IS_USED:""" ++ c.gl2Enabled.toString ++ """" \
+             "GL2_0_OUT_FREQ:""" ++ c.gl2_0_out_freq.toString ++ """" \
+            } -validate_rules 0 
 fix_vlnv_instance -component {""" ++ modulename ++"""} -library {} -name {""" ++ modulename ++"""} 
 open_smartdesign -design {""" ++ modulename ++"""}
 configure_design -component {""" ++ modulename ++"""} -library {}"""
