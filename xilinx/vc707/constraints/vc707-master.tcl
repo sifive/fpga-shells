aac1f934f4d37bee09c8c94f8b58867eff669ed7
Megan Wachs <megan@sifive.com>
2018-05-02 09:10:15 -0700

    VC707: mark jtag group as async

--- xilinx/vc707/constraints/vc707-master.tcl
+++ xilinx/vc707/constraints/vc707-master.tcl
@@ -10,16 +10,20 @@ set group_cl  [get_clocks -quiet {chiplink_b2c_clock              \
                                   clk_out*_vc707_sys_clock_mmcm3}]
 set group_pci [get_clocks -quiet -include_generated_clocks -of_objects [get_pins -hier -filter {name =~ *pcie*TXOUTCLK}]]
 
+set group_jtag [get_clocks -quiet {jtag_TCK}]
+
 puts "group_mem: $group_mem"
 puts "group_sys: $group_sys"
 puts "group_pci: $group_pci"
 puts "group_cl:  $group_cl"
+puts "group_jtag: $group_jtag"
 
 set groups [list]
-if { [llength $group_mem] > 0 } { lappend groups -group $group_mem }
-if { [llength $group_sys] > 0 } { lappend groups -group $group_sys }
-if { [llength $group_pci] > 0 } { lappend groups -group $group_pci }
-if { [llength $group_cl]  > 0 } { lappend groups -group $group_cl }
+if { [llength $group_mem]    > 0 } { lappend groups -group $group_mem }
+if { [llength $group_sys]    > 0 } { lappend groups -group $group_sys }
+if { [llength $group_pci]    > 0 } { lappend groups -group $group_pci }
+if { [llength $group_cl]     > 0 } { lappend groups -group $group_cl }
+if { [llength $group_cjtag]  > 0 } { lappend groups -group $group_jtag }
 
 puts "set_clock_groups -asynchronous $groups"
 set_clock_groups -asynchronous {*}$groups
