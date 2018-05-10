d52e708002da162e4a7fbb23af56d5a9c6464d9a
Behzad Boroujerdian <behzadboro@sifive.com>
2018-05-10 11:55:07 -0700

    chiplink: make input/output constraints conditional

--- xilinx/vc707/constraints/vc707-master.tcl
+++ xilinx/vc707/constraints/vc707-master.tcl
@@ -4,6 +4,22 @@ if { [llength [get_ports -quiet chiplink_b2c_clk]] > 0 } {
           -divide_by 1 \
           -source [ get_pins { vc707_sys_clock_mmcm0/inst/mmcm_adv_inst/CLKOUT6 } ] \
           [ get_ports { chiplink_c2b_clk } ]
+
+  # RX side: want to latch almost anywhere except on the rising edge of the clock
+  # The data signals coming from Aloe have: clock - 1.2 <= transition <= clock + 0.8
+  # Let's add 0.6ns of safety for trace jitter+skew on both sides:
+  #   min = hold           = - 1.2 - 0.6
+  #   max = period - setup =   0.8 + 0.6
+  set_input_delay -min -1.8 -clock {chiplink_b2c_clock} [ get_ports { chiplink_b2c_data* chiplink_b2c_rst chiplink_b2c_send } ]
+  set_input_delay -max  1.4 -clock {chiplink_b2c_clock} [ get_ports { chiplink_b2c_data* chiplink_b2c_rst chiplink_b2c_send } ]
+
+  # TX side: want to transition almost anywhere except on the rising edge of the clock
+  # The data signals going to Aloe must have: clock - 1.85 <= NO transition <= clock + 0.65
+  # Let's add 1ns of safey for trace jitter+skew on both sides:
+  #   min = -hold = -0.65 - 0.6
+  #   max = setup =  1.85 + 0.6
+  set_output_delay -min -1.25 -clock {chiplink_c2b_clock} [ get_ports { chiplink_c2b_data* chiplink_c2b_rst chiplink_c2b_send } ]
+  set_output_delay -max  2.45 -clock {chiplink_c2b_clock} [ get_ports { chiplink_c2b_data* chiplink_c2b_rst chiplink_c2b_send } ]
 }
 
 set group_mem [get_clocks -quiet {clk_pll_i}]
@@ -28,19 +44,3 @@ if { [llength $group_cl]  > 0 } { lappend groups -group $group_cl }
 
 puts "set_clock_groups -asynchronous $groups"
 set_clock_groups -asynchronous {*}$groups
-
-# RX side: want to latch almost anywhere except on the rising edge of the clock
-# The data signals coming from Aloe have: clock - 1.2 <= transition <= clock + 0.8
-# Let's add 0.6ns of safety for trace jitter+skew on both sides:
-#   min = hold           = - 1.2 - 0.6
-#   max = period - setup =   0.8 + 0.6
-set_input_delay -min -1.8 -clock {chiplink_b2c_clock} [ get_ports { chiplink_b2c_data* chiplink_b2c_rst chiplink_b2c_send } ]
-set_input_delay -max  1.4 -clock {chiplink_b2c_clock} [ get_ports { chiplink_b2c_data* chiplink_b2c_rst chiplink_b2c_send } ]
-
-# TX side: want to transition almost anywhere except on the rising edge of the clock
-# The data signals going to Aloe must have: clock - 1.85 <= NO transition <= clock + 0.65
-# Let's add 1ns of safey for trace jitter+skew on both sides:
-#   min = -hold = -0.65 - 0.6
-#   max = setup =  1.85 + 0.6
-set_output_delay -min -1.25 -clock {chiplink_c2b_clock} [ get_ports { chiplink_c2b_data* chiplink_c2b_rst chiplink_c2b_send } ]
-set_output_delay -max  2.45 -clock {chiplink_c2b_clock} [ get_ports { chiplink_c2b_data* chiplink_c2b_rst chiplink_c2b_send } ]
