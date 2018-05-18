dc3f798aa97c6fa11992288f67b67544ad8dd002
Behzad Boroujerdian <behzadboro@sifive.com>
2018-05-18 15:18:12 -0700

    pll generator is introduced for microsemi

--- src/main/scala/ip/microsemi/polarfire_ccc/PolarFireCCC.scala
+++ src/main/scala/ip/microsemi/polarfire_ccc/PolarFireCCC.scala
@@ -6,7 +6,9 @@ import chisel3.experimental.{Analog,attach}
 import freechips.rocketchip.util.{ElaborationArtefacts}
 import freechips.rocketchip.util.GenericParameterizedBundle
 import freechips.rocketchip.config._
+import sifive.fpgashells.clocks._
 
+/*
 case class PolarFireCCCParameters(
   name:             String,
   pll_in_freq:      Double  = 50,
@@ -24,52 +26,77 @@ case class PolarFireCCCParameters(
   gl3_0_pll_phase:  Double  = 0,
   feedback:         Boolean = false
 )
+*/
 
 // Black Box for Microsemi PolarFire Clock Conditioning Circuit (CCC) Actel:SgCore:PF_CCC:1.0.112
 
-class PolarFireCCCIOPads(c : PolarFireCCCParameters) extends Bundle {
+class PolarFireCCCIOPads(c : PLLParameters) extends Bundle {
     val REF_CLK_0      = Clock(INPUT)
-    val OUT0_FABCLK_0  = if (c.gl0Enabled) Some(Clock(OUTPUT)) else None
-    val OUT1_FABCLK_0  = if (c.gl1Enabled) Some(Clock(OUTPUT)) else None
-    val OUT2_FABCLK_0  = if (c.gl2Enabled) Some(Clock(OUTPUT)) else None
-    val OUT3_FABCLK_0  = if (c.gl3Enabled) Some(Clock(OUTPUT)) else None
+    val OUT0_FABCLK_0  = if (c.req.size >= 1) Some(Clock(OUTPUT)) else None
+    val OUT1_FABCLK_0  = if (c.req.size >= 2) Some(Clock(OUTPUT)) else None
+    val OUT2_FABCLK_0  = if (c.req.size >= 3) Some(Clock(OUTPUT)) else None
+    val OUT3_FABCLK_0  = if (c.req.size >= 4) Some(Clock(OUTPUT)) else None
     val PLL_LOCK_0     = Bool(OUTPUT)
 }
 
 //scalastyle:off
 //turn off linter: blackbox name must match verilog module
-class PolarFireCCC(c : PolarFireCCCParameters)(implicit val p:Parameters) extends BlackBox
-{
-  val modulename = c.name
+class PolarFireCCC(c : PLLParameters)(implicit val p:Parameters) extends BlackBox with PLL {
+  val moduleName = c.name
   override def desiredName = c.name
 
   val io = new PolarFireCCCIOPads(c)
+  
+  def getLocked = io.PLL_LOCK_0
+  def getClocks = Seq() ++ io.OUT0_FABCLK_0 ++ io.OUT1_FABCLK_0 ++ 
+                           io.OUT2_FABCLK_0 ++ io.OUT3_FABCLK_0 
+  
+  def getClockNames = Seq.tabulate (c.req.size) { i =>
+    s"${c.name}/inst/mmcm_adv_inst/CLKOUT${i}" 
+  }
+  
+  var elaborateArtefactsString= "";
+  var elaborateArtefactsString_temp= ""; //containg ctrl generate parameters
+  
+  elaborateArtefactsString_temp += s""" "PLL_IN_FREQ_0:${c.input.freqMHz}" \\
+  |""".stripMargin
+  for (i <- 0 until 4) {
+      elaborateArtefactsString_temp += (if (i < c.req.size) 
+          {s""" "GL${i.toString}_0_IS_USED:true" \\
+          |""".stripMargin} 
+          else 
+          {s""" "GL${i.toString}_0_IS_USED:false" \\
+          |""".stripMargin});
+    }
 
+  for (i <- 0 until c.req.size) {
+      val freq = c.req(i).freqMHz.toString();
+      val phase =  c.req(i).phaseDeg.toString();
+      val jitter = c.req(i).jitter.toString();
+      val phaseError =  c.req(i).phaseErrorDeg.toString();
+      val dutyCycle =  c.req(i).dutyCycle.toString();
+      
+      elaborateArtefactsString_temp += 
+        s""" "GL${i.toString}_0_OUT_FREQ:${freq}" \\
+        | "GL${i.toString}_0_PLL_PHASE:${phase}" \\
+        |""".stripMargin
+    }
+
+     elaborateArtefactsString_temp += s""" "PLL_FEEDBACK_MODE_0:${if (c.input.feedback) "External" else "Post-VCO"}" \\""" 
+
+  elaborateArtefactsString += s""" create_design -id Actel:SgCore:PF_CCC:1.0.112 -design_name {${moduleName}} -config_file {} -params {} -inhibit_configurator 0
+      | open_smartdesign -design {${moduleName}}
+      | configure_design -component {${moduleName}} -library {} 
+      | configure_vlnv_instance -component {${moduleName}} -library {} -name {${moduleName}_0} \\
+      | -params {${elaborateArtefactsString_temp}
+      | } -validate_rules 0 
+      | fix_vlnv_instance -component {${moduleName}} -library {} -name {${moduleName}_0} 
+      | open_smartdesign -design {${moduleName}} 
+      | configure_design -component {${moduleName}} -library {}""".stripMargin
+
+  
   ElaborationArtefacts.add(
-    "AddIPInstance." ++ modulename ++".libero.tcl",
-    """ 
-create_design -id Actel:SgCore:PF_CCC:1.0.112 -design_name {""" ++ modulename ++"""} -config_file {} -params {} -inhibit_configurator 0
-open_smartdesign -design {""" ++ modulename ++"""}
-configure_design -component {""" ++ modulename ++"""} -library {} 
-configure_vlnv_instance -component {""" ++ modulename ++"""} -library {} -name {""" ++ modulename ++"""_0} \
-    -params {"PLL_IN_FREQ_0:""" ++ c.pll_in_freq.toString ++ """" \
-             "PLL_FEEDBACK_MODE_0:""" ++ (if (c.feedback) "External" else "Post-VCO") ++ """" \
-             "GL0_0_IS_USED:""" ++ c.gl0Enabled.toString ++ """" \
-             "GL0_0_OUT_FREQ:""" ++ c.gl0_0_out_freq.toString ++ """" \
-             "GL0_0_PLL_PHASE:""" ++ c.gl0_0_pll_phase.toString ++ """" \
-             "GL1_0_IS_USED:""" ++ c.gl1Enabled.toString ++ """" \
-             "GL1_0_OUT_FREQ:""" ++ c.gl1_0_out_freq.toString ++ """" \
-             "GL1_0_PLL_PHASE:""" ++ c.gl1_0_pll_phase.toString ++ """" \
-             "GL2_0_IS_USED:""" ++ c.gl2Enabled.toString ++ """" \
-             "GL2_0_OUT_FREQ:""" ++ c.gl2_0_out_freq.toString ++ """" \
-             "GL2_0_PLL_PHASE:""" ++ c.gl2_0_pll_phase.toString ++ """" \
-             "GL3_0_IS_USED:""" ++ c.gl3Enabled.toString ++ """" \
-             "GL3_0_OUT_FREQ:""" ++ c.gl3_0_out_freq.toString ++ """" \
-             "GL3_0_PLL_PHASE:""" ++ c.gl3_0_pll_phase.toString ++ """" \
-            } -validate_rules 0 
-fix_vlnv_instance -component {""" ++ modulename ++"""} -library {} -name {""" ++ modulename ++"""_0} 
-open_smartdesign -design {""" ++ modulename ++"""}
-configure_design -component {""" ++ modulename ++"""} -library {}"""
-  )
+    s"""AddIPInstance.${moduleName}.libero.tcl""",
+    elaborateArtefactsString)
 }
 //scalastyle:on
