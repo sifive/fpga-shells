ff7fc94a1cd9c4109e6d7501677cbb96f675eac6
Sagar Karandikar <sagar.karandikar@sifive.com>
2018-07-16 16:30:52 -0700

    wip

--- src/main/scala/shell/UARTOverlay.scala
+++ src/main/scala/shell/UARTOverlay.scala
@@ -5,9 +5,10 @@ import chisel3._
 import freechips.rocketchip.config._
 import freechips.rocketchip.diplomacy._
 import sifive.blocks.devices.uart._
+import freechips.rocketchip.subsystem.{BaseSubsystem, PeripheryBus, PeripheryBusKey}
 
-case class UARTOverlayParams()(implicit val p: Parameters)
-case object UARTOverlayKey extends Field[Seq[DesignOverlay[UARTOverlayParams, ModuleValue[UARTPortIO]]]](Nil)
+case class UARTOverlayParams(beatBytes: Int)(implicit val p: Parameters)
+case object UARTOverlayKey extends Field[Seq[DesignOverlay[UARTOverlayParams, TLUART]]](Nil)
 
 // Tack on cts, rts signals available on some FPGAs. They are currently unused
 // by our designs.
@@ -18,19 +19,31 @@ class FPGAUARTPortIO extends UARTPortIO {
 
 abstract class UARTOverlay(
   val params: UARTOverlayParams)
-    extends IOOverlay[FPGAUARTPortIO, ModuleValue[UARTPortIO]]
+    extends IOOverlay[FPGAUARTPortIO, TLUART]
 {
   implicit val p = params.p
 
   def ioFactory = new FPGAUARTPortIO
 
-  val uartSource = BundleBridgeSource(() => new UARTPortIO)
-  val uartSink = shell { uartSource.sink }
+  private val divinit = (p(PeripheryBusKey).frequency / 115200).toInt
+  val uartParam = p(PeripheryUARTKey).map(_.copy(divisorInit = divinit))
+  val use_name = Some(s"uart_0")
 
-  val designOutput = InModuleBody { uartSource.out(0)._1 }
+//  val uartSource = BundleBridge()
+//  val uartSink = shell { uartSource }
+
+  val designOutput =   LazyModule(new TLUART(params.beatBytes, uartParam(0)).suggestName(use_name))
+
+
+
+
+
+
+
+
 
   shell { InModuleBody {
-    io.txd := uartSink.io.txd
+//    io.txd := designOutput.module.io.port.txd
 
     // Some FPGAs have this, we don't use it.
     io.rtsn := false.B
