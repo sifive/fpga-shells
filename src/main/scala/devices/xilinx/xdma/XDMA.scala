d9e8623eae708db2c7b76e028d1957c3b63a8c06
Sagar Karandikar <sagar.karandikar@sifive.com>
2018-08-02 13:16:21 -0700

    xdma: Add new Xilinx XDMA PCIe IP

---
+++ src/main/scala/devices/xilinx/xdma/XDMA.scala
@@ -0,0 +1,52 @@
+// See LICENSE for license details.
+package sifive.fpgashells.devices.xilinx.xdma
+
+import chisel3._
+import freechips.rocketchip.amba.axi4._
+import freechips.rocketchip.config.Parameters
+import freechips.rocketchip.diplomacy._
+import freechips.rocketchip.tilelink._
+import freechips.rocketchip.interrupts._
+import freechips.rocketchip.subsystem.{SubsystemClockCrossing, HasCrossing, AsynchronousCrossing, CacheBlockBytes}
+import sifive.fpgashells.ip.xilinx.xdma._
+
+class XDMA(c: XDMAParams)(implicit p: Parameters, val crossing: SubsystemClockCrossing = AsynchronousCrossing(8))
+  extends LazyModule with HasCrossing
+{
+  val imp = LazyModule(new DiplomaticXDMA(c))
+
+  val slave: TLInwardNode =
+    (imp.slave
+      := AXI4Buffer()
+      := AXI4UserYanker()
+      := AXI4Deinterleaver(p(CacheBlockBytes))
+      := AXI4IdIndexer(idBits=4)
+      := TLToAXI4(adapterName = Some("pcie-slave")))
+
+  val control: TLInwardNode =
+    (imp.control
+      := AXI4Buffer()
+      := AXI4UserYanker(capMaxFlight = Some(2))
+      := TLToAXI4()
+      := TLFragmenter(4, p(CacheBlockBytes), holdFirstDeny = true))
+
+  val master: TLOutwardNode =
+    (TLWidthWidget(8)
+      := AXI4ToTL()
+      := AXI4UserYanker(capMaxFlight=Some(16))
+      := AXI4Fragmenter()
+      := AXI4IdIndexer(idBits=2)
+      := imp.master)
+
+  val intnode: IntOutwardNode = imp.intnode
+
+  lazy val module = new LazyModuleImp(this) {
+    val io = IO(new Bundle {
+      val pads = new XDMAPads
+      val clocks = new XDMAClocks
+    })
+
+    io.pads   <> imp.module.io.pads
+    io.clocks <> imp.module.io.clocks
+  }
+}
---
+++ src/main/scala/devices/xilinx/xdma/package.scala
@@ -0,0 +1,10 @@
+// See LICENSE for license details.
+package sifive.fpgashells.devices.xilinx
+
+package object xdma
+{
+  type XDMAPads   = sifive.fpgashells.ip.xilinx.xdma.XDMAPads
+  type XDMAClocks = sifive.fpgashells.ip.xilinx.xdma.XDMAClocks
+  type XDMAParams = sifive.fpgashells.ip.xilinx.xdma.XDMAParams
+  val  XDMAParams = sifive.fpgashells.ip.xilinx.xdma.XDMAParams
+}
