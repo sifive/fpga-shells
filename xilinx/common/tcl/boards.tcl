b739da5a56246097dcce582cc0faf63cf748a2f2
Rajesh Varadharajan <rajeshrv@sifive.com>
2018-08-13 16:34:02 -0700

    Added the arty-a7-100 board to the namespace

--- xilinx/common/tcl/boards.tcl
+++ xilinx/common/tcl/boards.tcl
@@ -4,5 +4,6 @@ namespace eval ::program::boards {}
 
 set ::program::boards::spec [dict create \
 	arty            [dict create  iface spix4   size 16   bitaddr 0x0        memdev {n25q128-3.3v-spi-x1_x2_x4}] \
+	arty-a7-100     [dict create  iface spix4   size 16   bitaddr 0x0        memdev {n25q128-3.3v-spi-x1_x2_x4}] \
 	vc707           [dict create  iface bpix16  size 128  bitaddr 0x3000000  ] \
 	vcu118          [dict create  iface spix8   size 256  bitaddr 0x0        memdev {mt25qu01g-spi-x1_x2_x4_x8}]]
