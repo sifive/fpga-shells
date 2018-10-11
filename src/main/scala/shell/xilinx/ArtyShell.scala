16a4ad7e7a90f63fef03abf5baee2c3e63c7b77a
Mohit Wani <mohitwani@sifive.com>
2018-10-11 14:54:31 -0700

    added defaulSampleDel as the default sync stage delay.

--- src/main/scala/shell/xilinx/ArtyShell.scala
+++ src/main/scala/shell/xilinx/ArtyShell.scala
@@ -170,7 +170,7 @@ abstract class ArtyShell(implicit val p: Parameters) extends RawModule {
         dut.qspi(0),
         dut.clock,
         dut.reset,
-        syncStages = 0
+        syncStages = qspi_params.defaultSampleDel
       )
 
       IOBUF(qspi_sck, dut.qspi(0).sck)
