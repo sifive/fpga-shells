5dd1781c0ded742d7433c4f210e0ff59888fb794
Erik Danie <erik.danie@sifive.com>
2019-03-20 17:18:29 -0700

    fixing vsrc bb

--- microsemi/common/tcl/libero.tcl
+++ microsemi/common/tcl/libero.tcl
@@ -100,8 +100,8 @@ new_project -ondemand_build_dh 1 -location "$Proj" -name "$Prjname" -project_des
 import_files \
          -convert_EDN_to_HDL 0 \
          -hdl_source "$chisel_build_dir/$chisel_project.$chisel_config.v" \
-         -hdl_source "../../rocket-chip/vsrc/AsyncResetReg.v" \
-         -hdl_source "../../rocket-chip/vsrc/plusarg_reader.v"
+         -hdl_source "../../rocket-chip/src/main/resources/vsrc/AsyncResetReg.v" \
+         -hdl_source "../../rocket-chip/src/main/resources/vsrc/plusarg_reader.v"
 
 #
 # Execute all design entry scripts generated from Chisel flow.
