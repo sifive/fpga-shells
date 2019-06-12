90d7cea38f47a630f768ffeae8113d6a8b39d7cd
Megan Wachs <megan@sifive.com>
2019-06-12 09:31:45 -0700

    Arty 100T: enable use of JB

--- src/main/scala/shell/xilinx/ArtyShell.scala
+++ src/main/scala/shell/xilinx/ArtyShell.scala
@@ -80,6 +80,16 @@ abstract class ArtyShell(implicit val p: Parameters) extends RawModule {
   val ja_6         = IO(Analog(1.W))
   val ja_7         = IO(Analog(1.W))
 
+  // JB (Used for even more generic GPIOs)
+  val jb_0         = IO(Analog(1.W))
+  val jb_1         = IO(Analog(1.W))
+  val jb_2         = IO(Analog(1.W))
+  val jb_3         = IO(Analog(1.W))
+  val jb_4         = IO(Analog(1.W))
+  val jb_5         = IO(Analog(1.W))
+  val jb_6         = IO(Analog(1.W))
+  val jb_7         = IO(Analog(1.W))
+
   // JD (used for JTAG connection)
   val jd_0         = IO(Analog(1.W))  // TDO
   val jd_1         = IO(Analog(1.W))  // TRST_n
