67b4983b801e1d81feefa5476900c72519719d7d
Erik Danie <erik.danie@sifive.com>
2019-07-12 16:48:48 -0700

    remove extra param and add comment

--- src/main/scala/shell/GPIOOverlay.scala
+++ src/main/scala/shell/GPIOOverlay.scala
@@ -9,20 +9,20 @@ import freechips.rocketchip.tilelink.TLBusWrapper
 import freechips.rocketchip.interrupts.IntInwardNode
 import chisel3.experimental._
 
-case class GPIOOverlayParams(gpioParam: GPIOParams, controlBus: TLBusWrapper, intNode: IntInwardNode, width: Int)(implicit val p: Parameters)
+case class GPIOOverlayParams(gpioParam: GPIOParams, controlBus: TLBusWrapper, intNode: IntInwardNode)(implicit val p: Parameters)
 case object GPIOOverlayKey extends Field[Seq[DesignOverlay[GPIOOverlayParams, TLGPIO]]](Nil)
 
-class GPIOPortIO(width: Int = 4) extends Bundle {
+class FPGAGPIOPortIO(width: Int = 4) extends Bundle {
   val gpio = Vec(width, Analog(1.W))
 }
 
 abstract class GPIOOverlay(
   val params: GPIOOverlayParams)
-    extends IOOverlay[GPIOPortIO, TLGPIO]
+    extends IOOverlay[FPGAGPIOPortIO, TLGPIO]
 {
   implicit val p = params.p
 
-  def ioFactory = new GPIOPortIO(params.width)
+  def ioFactory = new FPGAGPIOPortIO(params.gpioParam.width)
   val tlgpio = GPIO.attach(GPIOAttachParams(params.gpioParam, params.controlBus, params.intNode))
 
   val tlgpioSink = shell { tlgpio.ioNode.makeSink }
