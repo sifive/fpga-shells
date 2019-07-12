c37298f150cdd230ac8abe3082be21edd47a6d52
reedafoster <reed.foster@sifive.com>
2019-07-12 14:51:34 -0700

    removed IDindexer and fixed device creation for DTS to remove duplication issue

--- src/main/scala/devices/xilinx/xilinxartyethernet/XilinxArtyEthernet.scala
+++ src/main/scala/devices/xilinx/xilinxartyethernet/XilinxArtyEthernet.scala
@@ -23,16 +23,15 @@ class XilinxArtyEthernetPads extends ArtyEthernetPrimaryIO {
 }
 
 class XilinxArtyEthernet(c: XilinxArtyEthernetParams)(implicit p: Parameters) extends LazyModule {
-  val device = new SimpleDevice("ethernetlite", Seq("sifive,ethernetlite0"))
+  val device = new SimpleDevice("ethernetlite", Seq("xlnx,axi-ethernetlite-3.0", "xlnx,xps-ethernetlite-1.00.a"))
 
   val island  = LazyModule(new XilinxArtyEthernetIsland(c, device.reg))
   val buffer  = LazyModule(new TLBuffer)
   val toaxi4  = LazyModule(new TLToAXI4(adapterName = Some("ethernet")))
-  val indexer = LazyModule(new AXI4IdIndexer(idBits = 1))
   val deint   = LazyModule(new AXI4Deinterleaver(p(CacheBlockBytes)))
   val yank    = LazyModule(new AXI4UserYanker)
 
-  island.crossAXI4In(island.node) := yank.node := deint.node := indexer.node := toaxi4.node := buffer.node
+  island.crossAXI4In(island.node) := yank.node := deint.node := toaxi4.node := buffer.node
   val node: TLInwardNode = buffer.node
 
   val intnode = IntSourceNode(IntSourcePortSimple(num=1, resources=device.int))
@@ -50,12 +49,12 @@ class XilinxArtyEthernet(c: XilinxArtyEthernetParams)(implicit p: Parameters) ex
   }
 }
 
-class XilinxArtyEthernetIsland(c: XilinxArtyEthernetParams, res: Seq[Resource])(implicit p: Parameters) extends LazyModule with CrossesToOnlyOneClockDomain {
+class XilinxArtyEthernetIsland(c: XilinxArtyEthernetParams, resource: Seq[Resource])(implicit p: Parameters) extends LazyModule with CrossesToOnlyOneClockDomain {
   val crossing = AsynchronousCrossing(8)
   val node = AXI4SlaveNode(Seq(AXI4SlavePortParameters(
     slaves = Seq(AXI4SlaveParameters(
       address = AddressSet.misaligned(c.baseAddress, 0x2000),
-      resources = (new SimpleDevice("ethernetlite", Seq("sifive,ethernetlite0"))).reg,
+      resources = resource,
       regionType = RegionType.UNCACHED,
       executable = false,
       supportsWrite = TransferSizes(1, 64),
@@ -85,35 +84,21 @@ class XilinxArtyEthernetIsland(c: XilinxArtyEthernetParams, res: Seq[Resource])(
     blackbox.io.s_axi_aresetn := io.s_axi_aresetn
 
     // connnect up AXI stuff
-    //blackbox.io.s_axi_awid	  := axi.aw.bits.id
     blackbox.io.s_axi_awaddr 	:= axi.aw.bits.addr
-    //blackbox.io.s_axi_awlen		:= axi.aw.bits.len
-    //blackbox.io.s_axi_awsize  := axi.aw.bits.size
-    //blackbox.io.s_axi_awburst	:= axi.aw.bits.burst
-    //blackbox.io.s_axi_awcache	:= axi.aw.bits.cache
     blackbox.io.s_axi_awvalid := axi.aw.valid
     axi.aw.ready              := blackbox.io.s_axi_awready
     blackbox.io.s_axi_wdata 	:= axi.w.bits.data
     blackbox.io.s_axi_wstrb 	:= axi.w.bits.strb
-    //blackbox.io.s_axi_wlast		:= axi.w.bits.last
     blackbox.io.s_axi_wvalid 	:= axi.w.valid
     axi.w.ready               := blackbox.io.s_axi_wready
-    //axi.b.bits.id             := blackbox.io.s_axi_bid
     axi.b.bits.resp           := blackbox.io.s_axi_bresp
     axi.b.valid               := blackbox.io.s_axi_bvalid
     blackbox.io.s_axi_bready 	:= axi.b.ready
-    //blackbox.io.s_axi_arid 		:= axi.ar.bits.id
     blackbox.io.s_axi_araddr 	:= axi.ar.bits.addr
-    //blackbox.io.s_axi_arlen		:= axi.ar.bits.len
-    //blackbox.io.s_axi_arsize  := axi.ar.bits.size
-    //blackbox.io.s_axi_arburst	:= axi.ar.bits.burst
-    //blackbox.io.s_axi_arcache	:= axi.ar.bits.cache
     blackbox.io.s_axi_arvalid := axi.ar.valid
     axi.ar.ready              := blackbox.io.s_axi_arready
-    //axi.r.bits.id             := blackbox.io.s_axi_rid
     axi.r.bits.data           := blackbox.io.s_axi_rdata
     axi.r.bits.resp           := blackbox.io.s_axi_rresp
-    //axi.r.bits.last           := blackbox.io.s_axi_rlast
     axi.r.valid               := blackbox.io.s_axi_rvalid
     blackbox.io.s_axi_rready 	:= axi.r.ready
   }
