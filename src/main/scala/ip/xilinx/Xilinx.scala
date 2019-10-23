6b8a1d074757ac1861a4b4e9ce361f4ba52adbe3
steve-shababi <steve.shababi@gamil.com>
2019-10-22 17:04:52 -0700

    Added JTAG Tunneling Translation Logic (for using FT2232 JTAG outputs for both bitstream flashing and coreip debugging)

--- src/main/scala/ip/xilinx/Xilinx.scala
+++ src/main/scala/ip/xilinx/Xilinx.scala
@@ -9,6 +9,154 @@ import freechips.rocketchip.util.{ElaborationArtefacts}
 import sifive.blocks.devices.pinctrl.{BasePin}
 import sifive.fpgashells.clocks._
 
+
+//=============================================================
+// Translation Logic for JTAG Tunneling which translates Sifive 
+// coreip JTAG protocol (used for internal debugging with gdb and openocd) 
+// to BSCAN JTAG protocol (used for Xilinx FPGA bitstream programming)
+// Note that an overaly should connect coreip JTAG protocols to 
+// Xilinx BSCAN resource using this module JTAGTUNNEL
+// ============================================================
+ 
+class JTAGTUNNEL extends BlackBox { 
+//class JTAGTUNNEL extends BlackBox with HasBlackBoxInline { 
+//class JTAGTUNNEL extends Module { 
+  val io = new Bundle { 
+    val jtag_tck = Output(Bool()) 
+    val jtag_tms = Output(Bool()) 
+    val jtag_tdi = Output(Bool()) 
+    val jtag_tdo = Input(Bool()) 
+    val jtag_tdo_en = Input(Bool()) 
+  } 
+  //setInline("JTAGTUNNEL.v", 
+  //  s""" 
+  ElaborationArtefacts.add("JTAGTUNNEL.v", 
+    """ 
+      |module JTAGTUNNEL( 
+      |    output jtag_tck, 
+      |    output jtag_tms, 
+      |    output jtag_tdi, 
+      |    input jtag_tdo, 
+      |    input jtag_tdo_en 
+      |); 
+      |  
+      |parameter USER_CHAIN=4; 
+      |wire CAPTURE; 
+      |wire DRCK; 
+      |wire RESET; 
+      |wire SEL; 
+      |wire SHIFT; 
+      |wire UPDATE; 
+      |wire RUNTEST; 
+      | 
+      |wire jtag_tck; 
+      |reg  jtag_tms; 
+      |wire jtag_tdi; 
+      |wire jtag_tdo; 
+      |wire jtag_tdo_en; 
+      |wire TDO; 
+      |wire reset; 
+      |wire start_tck; 
+      |reg [6:0] shiftreg_cnt; 
+      |reg [7:0] counter_neg; 
+      |reg [7:0] counter_pos; 
+      |reg         TDI_REG; 
+      | 
+      |BSCANE2 #( 
+      |  .JTAG_CHAIN(USER_CHAIN) // Value for USER command. 
+      |) 
+      |BSCANE2_inst ( 
+      |  .CAPTURE(CAPTURE), // 1-bit output: CAPTURE output from TAP controller. 
+      |  .DRCK(DRCK), // 1-bit output: Gated TCK output. When SEL is asserted, DRCK toggles when CAPTURE or 
+      |  // SHIFT are asserted. 
+      |    .RESET(RESET), // 1-bit output: Reset output for TAP controller. 
+      |    .RUNTEST(RUNTEST), // 1-bit output: Output asserted when TAP controller is in Run Test/Idle state. 
+      |    .SEL(SEL), // 1-bit output: USER instruction active output. 
+      |    .SHIFT(SHIFT), // 1-bit output: SHIFT output from TAP controller. 
+      |    .TCK(TCK), // 1-bit output: Test Clock output. Fabric connection to TAP Clock pin. 
+      |    .TDI(TDI), // 1-bit output: Test Data Input (TDI) output from TAP controller. 
+      |    .TMS(TMS), // 1-bit output: Test Mode Select output. Fabric connection to TAP. 
+      |    .UPDATE(UPDATE), // 1-bit output: UPDATE output from TAP controller 
+      |    .TDO(TDO) // 1-bit input: Test Data Output (TDO) input for USER function. 
+      |  ); 
+      |// 
+      |BUFGCE BUFGCE_JTAG ( 
+      |  .O(jtag_tck), // 1-bit output: Clock output 
+      |  .CE(start_tck), // 1-bit input: Clock enable input for I0 
+      |  .I(TCK) // 1-bit input: Primary clock 
+      |); 
+      | 
+      | 
+      |assign start_tck = SEL; 
+      |assign jtag_tdi = TDI; 
+      |assign TDO = jtag_tdo_en ? jtag_tdo : 1'b1; 
+      | 
+      |always@(*) begin 
+      |        if (counter_neg == 8'h04) begin 
+      |                jtag_tms = TDI_REG; 
+      |        end else if (counter_neg == 8'h05) begin 
+      |                jtag_tms = 1'b1; 
+      |        end else if ((counter_neg == (8'h08 + shiftreg_cnt)) || (counter_neg == (8'h08 + shiftreg_cnt - 8'h01))) begin 
+      |                jtag_tms = 1'b1; 
+      |        end else begin 
+      |                jtag_tms = 1'b0; 
+      |        end 
+      |end 
+      | 
+      |always@(posedge TCK) begin 
+      |        if (~SHIFT) begin 
+      |                shiftreg_cnt <= 7'b0000000; 
+      |        end else if ((counter_pos >= 8'h01) && (counter_pos <= 8'h07))  begin 
+      |                shiftreg_cnt <= {{TDI, shiftreg_cnt[6:1]}}; 
+      |        end else begin 
+      |                shiftreg_cnt <= shiftreg_cnt; 
+      |        end 
+      |end 
+      | 
+      |always@(posedge TCK) begin 
+      |        if (~SHIFT) begin 
+      |                TDI_REG <= 1'b0; 
+      |        end else if (counter_pos == 8'h00) begin 
+      |                TDI_REG <= ~TDI; 
+      |        end else begin 
+      |                TDI_REG <= TDI_REG; 
+      |        end 
+      |end 
+      | 
+      |always@(negedge TCK) begin 
+      |        if (~SHIFT) begin 
+      |                counter_neg <= 8'b00000000; 
+      |        end else begin 
+      |                counter_neg <= counter_neg + 1; 
+      |        end 
+      |end 
+      |  
+      |always@(posedge TCK) begin 
+      |        if (~SHIFT) begin 
+      |                counter_pos <= 8'b00000000; 
+      |        end else begin 
+      |                counter_pos <= counter_pos + 1; 
+      |        end 
+      |end 
+      |endmodule 
+    """.stripMargin) 
+}
+object JTAGTUNNEL { 
+ 
+    def apply (DUT_TCK: BasePin, DUT_TMS: BasePin, DUT_TDI: BasePin, DUT_TDO: BasePin): Unit = { 
+      val inst_jtag_tunnel = Module(new JTAGTUNNEL()) 
+      DUT_TCK.i.ival := inst_jtag_tunnel.io.jtag_tck 
+      DUT_TMS.i.ival := inst_jtag_tunnel.io.jtag_tms 
+      DUT_TDI.i.ival := inst_jtag_tunnel.io.jtag_tdi 
+      //attach(inst_jtag_tunnel.io.jtag_tdo, DUT_TDO) 
+      inst_jtag_tunnel.io.jtag_tdo := DUT_TDO.o.oval 
+      inst_jtag_tunnel.io.jtag_tdo_en := DUT_TDO.o.oe 
+  }
+}
+
+
+//======================================
+
 //========================================================================
 // This file contains common devices used by our Xilinx FPGA flows and some
 // BlackBox modules used in the Xilinx FPGA flows
@@ -126,7 +274,7 @@ class Series7MMCM(c : PLLParameters) extends BlackBox with PLLInstance {
   def getReset = Some(io.reset)
   def getLocked = io.locked
   def getClockNames = Seq.tabulate (c.req.size) { i =>
-    s"clk_out${i}_${c.name}"
+    s"clk_out${i+1}_${c.name}"
   }
 
   val used = Seq.tabulate(7) { i =>
