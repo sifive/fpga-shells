c608320e8a1fa42188842251cf9ba8b5b372dd21
Ryan Macdonald <rmac@sifive.com>
2020-02-11 13:54:52 -0800

    Remove controller inst from GPIO overlay

--- src/main/scala/shell/GPIOOverlay.scala
+++ src/main/scala/shell/GPIOOverlay.scala
@@ -14,14 +14,13 @@ import sifive.blocks.devices.gpio._
 
 //Might delete later...
 //Should GPIO be an overlay? Probably not, PinOverlay might take over this use case
-//Plus this should NOT place the controller
 case class GPIOShellInput()
 case class GPIODesignInput(
-  gpioParam: GPIOParams,
+  gpioParams: GPIOParams,
   controlBus: TLBusWrapper,
   intNode: IntInwardNode,
   parentLogicalTreeNode: Option[LogicalTreeNode] = None)(implicit val p: Parameters)
-case class GPIOOverlayOutput(gpio: TLGPIO)
+case class GPIOOverlayOutput(gpio: ModuleValue[GPIOPortIO])
 case object GPIOOverlayKey extends Field[Seq[DesignPlacer[GPIODesignInput, GPIOShellInput, GPIOOverlayOutput]]](Nil)
 trait GPIOShellPlacer[Shell] extends ShellPlacer[GPIODesignInput, GPIOShellInput, GPIOOverlayOutput]
 
@@ -35,12 +34,9 @@ abstract class GPIOPlacedOverlay(
 {
   implicit val p = di.p
 
-  def ioFactory = new ShellGPIOPortIO(di.gpioParam.width)
-  val tlgpio = GPIO.attach(GPIOAttachParams(
-    gpio = di.gpioParam,
-    controlBus = di.controlBus,
-    intNode = di.intNode,
-    parentLogicalTreeNode = di.parentLogicalTreeNode))
-  val tlgpioSink = shell { tlgpio.ioNode.makeSink }
-  def overlayOutput = GPIOOverlayOutput(gpio = tlgpio)
+  def ioFactory = new ShellGPIOPortIO(di.gpioParams.width)
+
+  val tlgpioSource = BundleBridgeSource(() => new GPIOPortIO(di.gpioParams))
+  val tlgpioSink = shell { tlgpioSource.makeSink }
+  def overlayOutput = GPIOOverlayOutput(gpio = InModuleBody{ tlgpioSource.bundle })
 }
