0e604d3c82cadb0bdd903be56263c912c5c52bf3
Jacka Tsai <jackat@gamma05.internal.sifive.com>
2020-03-18 20:04:22 -0700

    Remove PCIe IO standard constraints for E cores. To-Do: Add another constraint file for PCIe interface and include it if design needed.

--- xilinx/vc707/tcl/clocks.tcl
+++ xilinx/vc707/tcl/clocks.tcl
@@ -57,4 +57,4 @@ if { [llength $group_ulpi]   > 0 } { lappend groups -group $group_ulpi }
 puts "set_clock_groups -asynchronous $groups"
 set_clock_groups -asynchronous {*}$groups
 
-set_false_path -from [get_clocks JTCK] -to [get_clocks -of_objects [get_pins lazysys/xilinxvc707mig_1/island/blackbox/u_vc707mig4gb_mig/u_iodelay_ctrl/clk_ref_mmcm_gen.mmcm_i/CLKOUT1]]
+#set_false_path -from [get_clocks JTCK] -to [get_clocks -of_objects [get_pins lazysys/xilinxvc707mig_1/island/blackbox/u_vc707mig4gb_mig/u_iodelay_ctrl/clk_ref_mmcm_gen.mmcm_i/CLKOUT1]]
--- xilinx/vc707/tcl/ios.tcl
+++ xilinx/vc707/tcl/ios.tcl
@@ -48,16 +48,16 @@ set_property IOB TRUE [get_ports uart_rtsn]
 
 # PCI Express
 #FMC 1 refclk
-set_property PACKAGE_PIN A10 [get_ports {pcie_REFCLK_rxp}]
-set_property PACKAGE_PIN A9 [get_ports {pcie_REFCLK_rxn}]
-create_clock -name pcie_ref_clk -period 10 [get_ports pcie_REFCLK_rxp]
-set_input_jitter [get_clocks -of_objects [get_ports pcie_REFCLK_rxp]] 0.5
+#set_property PACKAGE_PIN A10 [get_ports {pcie_REFCLK_rxp}]
+#set_property PACKAGE_PIN A9 [get_ports {pcie_REFCLK_rxn}]
+#create_clock -name pcie_ref_clk -period 10 [get_ports pcie_REFCLK_rxp]
+#set_input_jitter [get_clocks -of_objects [get_ports pcie_REFCLK_rxp]] 0.5
 
-set_property PACKAGE_PIN H4 [get_ports {pcie_pci_exp_txp}]
-set_property PACKAGE_PIN H3 [get_ports {pcie_pci_exp_txn}]
+#set_property PACKAGE_PIN H4 [get_ports {pcie_pci_exp_txp}]
+#set_property PACKAGE_PIN H3 [get_ports {pcie_pci_exp_txn}]
 
-set_property PACKAGE_PIN G6 [get_ports {pcie_pci_exp_rxp}]
-set_property PACKAGE_PIN G5 [get_ports {pcie_pci_exp_rxn}]
+#set_property PACKAGE_PIN G6 [get_ports {pcie_pci_exp_rxp}]
+#set_property PACKAGE_PIN G5 [get_ports {pcie_pci_exp_rxn}]
 
 # SDIO
 set_property -dict { PACKAGE_PIN AN30  IOSTANDARD LVCMOS18  IOB TRUE } [get_ports {sdio_clk}]
