9b4bd2b48becefc9e00f3cb62de72a9dc19416a4
Ryan Macdonald <rmac@sifive.com>
2020-08-25 14:10:11 -0700

    SDIO: Refactor overlay classes to be more generic for SPI IO

--- src/main/scala/shell/SDIOOverlay.scala
+++ src/main/scala/shell/SDIOOverlay.scala
@@ -11,60 +11,30 @@ import freechips.rocketchip.tilelink.TLBusWrapper
 import freechips.rocketchip.interrupts.IntInwardNode
 
 //This should not do the controller placement either
-case class SDIOShellInput()
-case class SDIODesignInput(spiParam: SPIParams, node: BundleBridgeSource[SPIPortIO])(implicit val p: Parameters)
-case class SDIOOverlayOutput()
-case object SDIOOverlayKey extends Field[Seq[DesignPlacer[SDIODesignInput, SDIOShellInput, SDIOOverlayOutput]]](Nil)
-trait SDIOShellPlacer[Shell] extends ShellPlacer[SDIODesignInput, SDIOShellInput, SDIOOverlayOutput]
+case class SPIShellInput()
+case class SPIDesignInput(spiParam: SPIParams, node: BundleBridgeSource[SPIPortIO])(implicit val p: Parameters)
+case class SPIOverlayOutput()
+case object SPIOverlayKey extends Field[Seq[DesignPlacer[SPIDesignInput, SPIShellInput, SPIOverlayOutput]]](Nil)
+trait SPIShellPlacer[Shell] extends ShellPlacer[SPIDesignInput, SPIShellInput, SPIOverlayOutput]
 
-// SDIO Port. Not sure how generic this is, it might need to move.
-class ShellSDIOPortIO extends Bundle {
-  val sdio_clk = Output(Bool())
-  val sdio_cmd = Output(Bool())
-  val sdio_dat_0 = Input(Bool())
-  val sdio_dat_1 = Analog(1.W)
-  val sdio_dat_2 = Analog(1.W)
-  val sdio_dat_3 = Output(Bool())
+// SPI Port. Not sure how generic this is, it might need to move.
+class ShellSPIPortIO extends Bundle {
+  val spi_clk = Analog(1.W)
+  val spi_cs = Analog(1.W)
+  val spi_dat = Vec(4, Analog(1.W))
 }
 
-abstract class SDIOPlacedOverlay(
-  val name: String, val di: SDIODesignInput, val si: SDIOShellInput)
-    extends IOPlacedOverlay[ShellSDIOPortIO, SDIODesignInput, SDIOShellInput, SDIOOverlayOutput]
+abstract class SPIPlacedOverlay(
+  val name: String, val di: SPIDesignInput, val si: SPIShellInput)
+    extends IOPlacedOverlay[ShellSPIPortIO, SPIDesignInput, SPIShellInput, SPIOverlayOutput]
 {
   implicit val p = di.p
 
-  def ioFactory = new ShellSDIOPortIO
+  def ioFactory = new ShellSPIPortIO
   val tlspiSink = di.node.makeSink
 
   val spiSource = BundleBridgeSource(() => new SPIPortIO(di.spiParam))
   val spiSink = shell { spiSource.makeSink }
-  def overlayOutput = SDIOOverlayOutput()
+  def overlayOutput = SPIOverlayOutput()
 
-  InModuleBody {
-    val (io, _) = spiSource.out(0)
-    val tlspiport = tlspiSink.bundle
-    io :<> tlspiport
-    (0 to 3).foreach { case q =>
-      tlspiport.dq(q).i := RegNext(RegNext(io.dq(q).i))
-    }
-  }
-
-  shell { InModuleBody {
-    val sd_spi_sck = spiSink.bundle.sck
-    val sd_spi_cs = spiSink.bundle.cs(0)
-
-    val sd_spi_dq_i = Wire(Vec(4, Bool()))
-    val sd_spi_dq_o = Wire(Vec(4, Bool()))
-
-    spiSink.bundle.dq.zipWithIndex.foreach {
-      case(pin, idx) =>
-        sd_spi_dq_o(idx) := pin.o
-        pin.i := sd_spi_dq_i(idx)
-    }
-
-    io.sdio_clk := sd_spi_sck
-    io.sdio_dat_3 := sd_spi_cs
-    io.sdio_cmd := sd_spi_dq_o(0)
-    sd_spi_dq_i := Seq(false.B, io.sdio_dat_0, false.B, false.B)
-  } }
 }
--- src/main/scala/shell/xilinx/SDIOOverlay.scala
+++ src/main/scala/shell/xilinx/SDIOOverlay.scala
@@ -6,8 +6,36 @@ import freechips.rocketchip.diplomacy._
 import sifive.fpgashells.shell._
 import sifive.fpgashells.ip.xilinx._
 
-abstract class SDIOXilinxPlacedOverlay(name: String, di: SDIODesignInput, si: SDIOShellInput)
-  extends SDIOPlacedOverlay(name, di, si)
+abstract class SDIOXilinxPlacedOverlay(name: String, di: SPIDesignInput, si: SPIShellInput)
+  extends SPIPlacedOverlay(name, di, si)
 {
   def shell: XilinxShell
+
+  InModuleBody {
+    val (io, _) = spiSource.out(0)
+    val tlspiport = tlspiSink.bundle
+    io <> tlspiport
+    (0 to 3).foreach { case q =>
+      tlspiport.dq(q).i := RegNext(RegNext(io.dq(q).i))
+    }
+  }
+
+  shell { InModuleBody {
+    val sd_spi_sck = spiSink.bundle.sck
+    val sd_spi_cs = spiSink.bundle.cs(0)
+
+    val sd_spi_dq_i = Wire(Vec(4, Bool()))
+    val sd_spi_dq_o = Wire(Vec(4, Bool()))
+
+    spiSink.bundle.dq.zipWithIndex.foreach {
+      case(pin, idx) =>
+        sd_spi_dq_o(idx) := pin.o
+        pin.i := sd_spi_dq_i(idx)
+    }
+
+    UIntToAnalog(sd_spi_sck, io.spi_clk, true.B)
+    UIntToAnalog(sd_spi_cs, io.spi_dat(3), true.B)
+    UIntToAnalog(sd_spi_dq_o(0), io.spi_cs, true.B)
+    sd_spi_dq_i := Seq(false.B, AnalogToUInt(io.spi_dat(0)).asBool, false.B, false.B)
+  } }
 }
